64c5ca0fb8c4539877c74d231f324ede6cee3c44
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-17 00:39:16 +0530

    fix: PORT Mapping

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res)=> {
+app.get('/health', (req, res)=> {
     console.log("Server is healthy");
 })
 
 app.post('/post', emailController);
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log("server running on port 5000")
+    console.log(`server running on port ${PORT}`)
 })
 
